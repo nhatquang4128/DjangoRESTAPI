47bfa1d460bd21744bea159ebe7945c2d4369c2c
nhatquang4128 <168263278+nhatquang4128@users.noreply.github.com>
2026-01-30 12:39:21 -0500

    Implemented JWT auth, refresh token, and protected CRUD endpoints

---
+++ blog/views.py
@@ -0,0 +1,55 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Post
+from .serializers import PostSerializer
+
+# Create your views here.
+@api_view(['GET', 'POST'])
+def post_list(request):
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        if not request.user.is_authenticated:
+            return Response(
+                {'detail': 'Authentication Credentials were not provided.'},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+        serializer = PostSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def post_detail(request, pk):
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        return Response(
+            {'error': 'Post not found.'},
+            status=status.HTTP_404_NOT_FOUND
+        )
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    if not request.user.is_authenticated:
+        return Response(
+            {'detail': 'Authentication Credentials were not provided.'},
+            status=status.HTTP_401_UNAUTHORIZED
+        )
+    if request.method == 'PUT':
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method =='DELETE':
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
