cfc2f8259b617076fe5394c4ae25cc3f136a247a
nhatquang4128 <168263278+nhatquang4128@users.noreply.github.com>
2026-02-23 14:50:44 -0500

    Added postgresql support and docker configuration

--- todolist/settings.py
+++ todolist/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'notes_db',
+        'USER': 'huyhuynh',  # usually your Mac username
+        'PASSWORD': '',  # leave empty for local development
+        'HOST': 'host.docker.internal',
+        'PORT': '5432',
     }
 }
 
